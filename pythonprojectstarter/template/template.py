9817c78ce72209719983a087734e7e39216dfb76
Cosmin Craciun <cosmin.craciun@cwavesoftware.com>
2023-02-02 15:10:44 +0200

    adding some agparser examples

--- pythonprojectstarter/template/template.py
+++ pythonprojectstarter/template/template.py
@@ -4,7 +4,13 @@ import argparse
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a %d %b %Y %H:%M:%S')
 
 parser = argparse.ArgumentParser(description='TBD')
+'''
+# more examples here: https://docs.python.org/3/library/argparse.html
 
+parser.add_argument('filename')           # positional argument
+parser.add_argument('-c', '--count')      # option that takes a value
+parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
+'''
 args = parser.parse_args()
 
 if __name__ == "__main__":
