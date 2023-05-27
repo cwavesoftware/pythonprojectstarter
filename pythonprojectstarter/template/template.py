c93af162a16ba6ba98dc336f9d17e203428dc40a
Cosmin Craciun <cosmin.craciun@cwavesoftware.com>
2023-05-27 15:53:01 +0300

    Adding logging level in template

--- pythonprojectstarter/template/template.py
+++ pythonprojectstarter/template/template.py
@@ -1,9 +1,8 @@
 import logging
 import argparse
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a %d %b %Y %H:%M:%S')
-
 parser = argparse.ArgumentParser(description='TBD')
+parser.add_argument('--log-level')
 '''
 # more examples here: https://docs.python.org/3/library/argparse.html
 
@@ -12,6 +11,15 @@ parser.add_argument('-c', '--count')      # option that takes a value
 parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
 '''
 args = parser.parse_args()
+
+if args.log_level == 'debug':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a %d %b %Y %H:%M:%S')
+elif args.log_level == 'info':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a %d %b %Y %H:%M:%S')
+elif args.log_level == 'warning':
+    logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a %d %b %Y %H:%M:%S')
+elif args.log_level == 'error':
+    logging.basicConfig(level=logging.ERROR, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a %d %b %Y %H:%M:%S')
 
 if __name__ == "__main__":
     logging.info("Done")
